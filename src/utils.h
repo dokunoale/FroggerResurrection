49d6a5b4c086b10083b90559d21b6fb211d9ca2b
dokunoale <153988414+dokunoale@users.noreply.github.com>
2024-12-28 15:42:13 +0100

    leggibilità

--- src/utils.h
+++ src/utils.h
@@ -7,9 +7,6 @@
 #include "processes.h"
 
 
-int rand_num (int min, int max)
-{
-    return rand() % (max-min) + min;
-}
+int choose(int min, int max) { return rand() % (max - min + 1) + min; }
 
 #endif // UTILS_H
